44314a3c94485b42f6bdc7aaf36957bdfbe743d7
Wator1704 <HoangBlue1704@gmail.com>
2025-05-22 08:50:35 +0700

    update

--- src/App.jsx
+++ src/App.jsx
@@ -60,45 +60,118 @@ function App() {
     setData(words ?? [])
     setIndexLearn(0)
   }
+
+  // Create a folder
+  const [showCreateFolder, setShowCreateFolder] = useState(false)
+  const createFolder = () => {
+    setShowCreateFolder(true)
+  }
+
+  const [folderName, setFolderName] = useState('')
+  const [folders, setFolders] = useState(() => {
+    const localFolders = JSON.parse(localStorage.getItem('folders'))
+    return localFolders ?? []
+  })
+
+  const handleFolderNameChange = () => {
+    if (folderName != '') {
+      setFolders(prev => {
+        const newFolders = [...prev, folderName]
+        const jsonFolders = JSON.stringify(newFolders)
+    
+        localStorage.setItem('folders', jsonFolders)
+
+        return newFolders
+      })
+      
+    }
+
+    setFolderName('')
+    setShowCreateFolder(false)
+  }
+
+  const [showWordSection, setShowWordSection] = useState(false)
+
+  const openWordSection = () => {
+    setShowWordSection(true)
+    setShowCreateFolder(false)
+  }
+
+  const quitWordSection = () => {
+    setShowWordSection(false)
+  }
+
   return (
     <div className="main">
-      <div className="word-section">
-
-        <input 
-          type="text"
-          placeholder='Enter word'
-          value={word}
-          onChange={e => setWord(e.target.value)} 
-        />
-        <input 
-          type="text"
-          placeholder='Enter meaning'
-          value={meaning}
-          onChange={e => setMeaning(e.target.value)} 
-        />
-
-        <button onClick={addWord}>Add</button>
-        <ul>
-            {
-              words.map((word, index) => 
-                <li key={index}>
-                  <h3>{word.name}</h3>
-                  <p>{word.mean}</p>
-                  <button 
-                  className='deleteBtn'
-                  onClick={() => {
-                    const newWords = words.filter((words, i) => i !== index)
-                    localStorage.setItem('words', JSON.stringify(newWords))
-                    setWords(newWords)
-                  }}>Delete</button>
-                </li>
-              )
-            }
-            </ul>
-        
-
-        <button className='learnBtn' onClick={learnBtn}>Learn</button>
+      <div className="create-folder-section">
+        <button className='add-folder-btn' onClick={createFolder}>+</button>
+        {showCreateFolder && (
+          <div className="create-folder-field">
+            <h3>Create a folder</h3>
+            <input 
+              type="text" 
+              placeholder='Enter folder name' 
+              onChange={e => setFolderName(e.target.value)}
+              value={folderName}
+            />
+            <button onClick={handleFolderNameChange}>Add folder</button>
+          </div>
+        )}
       </div>
+
+      <div className="folder-list">
+        {folders.map((folder, index) => (
+          <div className="folder" key={index} onClick={openWordSection}>{folder}</div>
+        ))}
+      </div>
+      
+      {showWordSection && (
+        <div className="word-section">
+          <div className="word-section-header">
+            <button onClick={quitWordSection}>Back</button>
+          </div>
+          <div className="word-section-body">
+            <div className="word-section-left">
+              <input 
+                type="text"
+                placeholder='Enter word'
+                value={word}
+                onChange={e => setWord(e.target.value)} 
+              />
+              <input 
+                type="text"
+                placeholder='Enter meaning'
+                value={meaning}
+                onChange={e => setMeaning(e.target.value)} 
+              />
+
+              <button onClick={addWord}>Add</button>
+
+              <button className='learnBtn' onClick={learnBtn}>Learn</button>
+            </div>
+
+            <div className="word-section-right">
+              <ul>
+                {
+                  words.map((word, index) => 
+                    <li key={index}>
+                      <h3>{word.name}</h3>
+                      <p>{word.mean}</p>
+                      <button 
+                      className='deleteBtn'
+                      onClick={() => {
+                        const newWords = words.filter((words, i) => i !== index)
+                        localStorage.setItem('words', JSON.stringify(newWords))
+                        setWords(newWords)
+                      }}>Delete</button>
+                    </li>
+                  )
+                }
+              </ul>
+            </div>
+          </div>
+        </div>
+      )}
 
       {showLearn && 
         <div className="learn-section">
